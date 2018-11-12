a576bb3ad5e4272299a7143cd0507ab085c08b5e
EduardoFreitas <eduardo.freitas.cp@gmail.com>
2018-11-12 16:43:50 -0200

    improving code

--- controllers.js
+++ controllers.js
@@ -1,25 +1,23 @@
 //Controllers
-weatherApp.controller('homeController', ['$scope', 'cityService', function ($scope, cityService) {
+weatherApp.controller('homeController', ['$scope','$location', 'cityService', function ($scope, $location, cityService) {
     $scope.city = cityService.city;
     $scope.$watch('city', function () {
         cityService.city = $scope.city;
-    })
+    });
+    
+    $scope.submit = function () {
+        $location.path('/forecast');
+    };
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function ($scope, $resource, $routeParams, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$routeParams', 'cityService', 'weatherService', function ($scope, $routeParams, cityService, weatherService) {
     $scope.city = cityService.city;
 
     $scope.hours = $routeParams.hours || 2;
 
-    $scope.weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast', {
-        callback: "JSON_CALLBACK"
-    }, {get: {method: "JSONP"}})
+
     // Separete file with keyForecast value
-    $scope.weatherResult = $scope.weatherAPI.get({
-        q: $scope.city,
-        cnt: $scope.hours,
-        APPID: keyForecast
-    });
+    $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.hours);
 
     $scope.convertToCelsius = function (degK) {
         return Math.round((degK - 273.15) * 100) / 100;
--- services.js
+++ services.js
@@ -1,4 +1,19 @@
 // Services
 weatherApp.service('cityService', function () {
     this.city = "Miami"
-})
+});
+
+weatherApp.service('weatherService', ['$resource', function ($resource) {
+    this.GetWeather = function (city, hours) {
+        var weatherAPI = $resource('http://api.openweathermap.org/data/2.5/forecast', {
+            callback: "JSON_CALLBACK"
+        }, {get: {method: "JSONP"}});
+
+        // Separete file with keyForecast value
+        return weatherAPI.get({
+            q: city,
+            cnt: hours,
+            APPID: keyForecast
+        });
+    }
+}]);
